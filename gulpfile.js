9fb67fb87ee1dc756bd5515afeba12c6c2811319
tao.zeng <tao.zeng.zt@gmail.com>
2016-03-02 23:32:27 +0800

    update package

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var fs = require('fs'),
   gulpWebpack = require('gulp-webpack'),
   WebpackDevServer = require('webpack-dev-server'),
   mkcfg = require('./tool/make.webpack.js'),
-  jasmine = require('gulp-jasmine'),
   karma = require('karma').Server,
   main = {
     src: './src',
